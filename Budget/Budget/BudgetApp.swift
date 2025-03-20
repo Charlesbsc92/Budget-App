075697b28d84869a285f70a1d4fadbc78c9027fc
Charles <charles@Charless-Mac-mini.local>
2025-03-20 15:46:51 +0530

    Budget App initial setup

---
+++ Budget/Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Charles on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget/Budget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget
+//
+//  Created by Charles on 20/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
